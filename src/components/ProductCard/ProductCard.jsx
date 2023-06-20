6c39e24e7faff4a72671fb772d361bee462ca04f
Vítor Arimatéa <vitorodev@outlook.com>
2023-06-20 10:11:08 -0300

    update: Adição de produtos ao carrinho criada

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types';
 
 import './ProductCard.css';
 
 import formatCurrency from '../../utils/formatCurrency';
+import ShoppingContext from '../../context/ShoppingContext';
 
 function ProductCard({ data }) {
 
   const { price, title, thumbnail } = data;
+
+  const { cartItems, setCartItems } = useContext(ShoppingContext);
+
+  const handleAddCart = () => {setCartItems([ ...cartItems, data ]);};
+  
+  /* Outra forma de realizar essa mesma função, porém bem maior 
+    const updatedCartItems = cartItems;
+
+    updatedCartItems.push(data);
+
+    setCartItems(updatedCartItems);
+  */
 
   return (
     <section className="product-card">
@@ -24,7 +37,11 @@ function ProductCard({ data }) {
         <h2 className="card__title">{title}</h2>
       </div>
 
-      <button type="button" className="button__add-cart">
+      <button 
+        type="button" 
+        className="button__add-cart"
+        onClick={handleAddCart}
+      >
         Adicionar ao carrinho
       </button>
     </section>
