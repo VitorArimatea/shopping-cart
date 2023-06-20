5d279971af030d74c5bb21e835d873adb30a92d4
Vítor Arimatéa <vitorodev@outlook.com>
2023-06-20 10:21:09 -0300

    update: Criando evento de click para abrir e fechar o cart

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { HiOutlineShoppingCart } from 'react-icons/hi';
 
 import './CartButton.css';
 
+import ShoppingContext from '../../context/ShoppingContext';
+
 function CartButton() {
+
+  const { cartItems, isCartVisible, setIsCartVisible } = useContext(ShoppingContext);
+
   return ( 
-    <button type="button" className="cart__button">
+    <button 
+      type="button" 
+      className="cart__button"
+      onClick={() => setIsCartVisible(!isCartVisible) }
+    >
       <HiOutlineShoppingCart />
-      <span className="cart-status">1</span>
+      { cartItems.length > 0 && <span className="cart-status">{cartItems.length}</span>}
     </button>
   );
 }
