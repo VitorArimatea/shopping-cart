bda97e2a8d3daa227b6e1a5214629f9266bdc487
Vítor Arimatéa <vitorodev@outlook.com>
2023-06-20 10:03:38 -0300

    update: Adicionando componente Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import Provider from './context/Provider';
+
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
 
-import Provider from './context/Provider';
 
 function App() {
   return (
     <Provider> 
       <Header />
-      <Products />    
+      <Products />   
+      <Cart /> 
     </Provider>
   );
 }
