e7279fc0aae5d30fc4b53858b7f17f02afb56df3
Vítor Arimatéa <vitorodev@outlook.com>
2023-06-20 10:17:42 -0300

    update: Novos useStates adicionados ao Provider

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -6,10 +6,19 @@ import ShoppingContext from './ShoppingContext';
 function Provider({ children }) {
   
   const [products, setProducts] = useState([]);
-  
+  const [cartItems, setCartItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
   const value = {
     products,
-    setProducts
+    setProducts,
+    loading, 
+    setLoading,
+    cartItems, 
+    setCartItems,
+    isCartVisible, 
+    setIsCartVisible,
   };
 
   return (
